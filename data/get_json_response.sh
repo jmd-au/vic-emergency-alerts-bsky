080495332a8576fc1d76f0c667acbe0b36cae464
James MacLeod <james@jmd.id.au>
2026-01-09 17:17:40 +1100

    chore: updating temporary local data scripts

--- data/get_json_response.sh
+++ data/get_json_response.sh
@@ -6,6 +6,7 @@ function get_json_response() {
     local path=$1
     curl -X GET \
         $SOURCE_URL \
+        -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36 Edg/143.0.0.0" \
         -o $path/response.json
 }
 
