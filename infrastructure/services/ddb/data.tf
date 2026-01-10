d9b6d7982e12f4f66ad588e5495a4440eda16d4e
James MacLeod <james@jmd.id.au>
2026-01-10 16:18:17 +1100

    chore: cleaning up ddb folder

--- infrastructure/services/ddb/data.tf
+++
@@ -1,6 +0,0 @@
-# resource "terraform_data" "pre_load_forms" {
-#   provisioner "local-exec" {
-#     interpreter = [ "python" ]
-#     command = "python ${path.module}/preload_forms.py"
-#   }
-# }
